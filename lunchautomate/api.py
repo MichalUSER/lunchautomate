6de07f564ef99ba069f03ca1541d0df2b5929ddc
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-04-19 11:12:30 +0200

    feat: `/choose_lunch` endpoint

--- lunchautomate/api.py
+++ lunchautomate/api.py
@@ -3,13 +3,14 @@ from django.http import HttpRequest, HttpResponse
 from ninja import NinjaAPI, Form
 from ninja.renderers import BaseRenderer
 from edupage_api import Edupage, Lunch
+from typing import Literal
+from datetime import datetime
+import time
 
 from .schemas import UserIn
 from .auth import GlobalAuth
 from .types import Request
 
-from datetime import datetime
-import time
 
 
 class ORJSONRenderer(BaseRenderer):
@@ -47,3 +48,13 @@ def lunches(request: Request, date: float = time.time()):
     if not isinstance(lunches, Lunch):
         return {"menus": {}}
     return {"menus": lunches.menus}
+
+@api.get("/choose_lunch")
+def choose_lunch(request: Request, date: float = time.time(), number: int = 1):
+    lunches = request.auth.get_lunches(datetime.fromtimestamp(date))
+    if number == 0:
+        lunches.sign_off(request.auth)
+    elif number > 3:
+        return api.create_response(request, {"message": "Invalid lunch number"}, status=403)
+    else:
+        lunches.choose(request.auth, number)
