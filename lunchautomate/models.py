7398bb0075e8dffdc9924b33f8e03dba646fe765
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-05-22 22:05:05 +0200

    feat: Setup postgres

--- lunchautomate/models.py
+++ lunchautomate/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class EdupageUser(models.Model):
     username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
     subdomain = models.CharField(max_length=50)
 
     def __str__(self):
--- lunchautomate/settings.py
+++ lunchautomate/settings.py
@@ -12,10 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -84,8 +90,12 @@ WSGI_APPLICATION = "lunchautomate.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USERNAME"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": "",
     }
 }
 
